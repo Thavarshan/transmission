abe296650e1adcd9166c4af6ef2779af0891da3c
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2023-07-12 17:03:24 +0100

    Update for docker web service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,23 +2,31 @@
 
 namespace App\Providers;
 
+use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
+     *
+     * @return void
      */
     public function register(): void
     {
-        //
     }
 
     /**
      * Bootstrap any application services.
+     *
+     * @param \Illuminate\Routing\UrlGenerator $url
+     *
+     * @return void
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
-        //
+        if ('production' == env('APP_ENV')) {
+            $url->forceScheme('https');
+        }
     }
 }
