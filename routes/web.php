c57cde321cdc4e9983c802e68bdcb7e7b8c37bc5
Jerome Thayananthajothy <tjthavarshan@gmail.com>
2023-07-14 06:29:37 +0100

    Cleanup routes

--- routes/web.php
+++ routes/web.php
@@ -6,15 +6,19 @@ use App\Http\Controllers\VehicleController;
 
 Route::get('/', fn () => redirect()->route('login'));
 
-Route::get('/home', [VehicleController::class, 'index'])->middleware(['auth'])->name('home');
+Route::get('/home', [VehicleController::class, 'index'])
+    ->middleware(['auth'])->name('home');
 Route::resource('vehicles', VehicleController::class, [
-    'except' => ['create', 'edit'],
+    'only' => ['store', 'update', 'destroy'],
 ])->middleware(['auth']);
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
